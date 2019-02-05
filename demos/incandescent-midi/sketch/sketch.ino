5060d3861b0233635ac75708c062fa7650016346
John French <john.french@pushgig.com>
2019-02-05 16:02:04 -0600

    set up ramps

--- demos/incandescent-midi/sketch/sketch.ino
+++ demos/incandescent-midi/sketch/sketch.ino
@@ -1,9 +1,19 @@
 #include <MIDI.h>  // Add Midi Library
+#include <Ramp.h> // Add RAMP library
 
 #define LED 13    // Arduino Board LED is on Pin 13
 
 //Create an instance of the library with default name, serial port and settings
 MIDI_CREATE_DEFAULT_INSTANCE();
+
+// Create a Ramp instance for each light so we can transition values smoothly
+ramp ramps[5];
+
+// map (interpolate) floating values
+// double mapf(double x, double in_min, double in_max, double out_min, double out_max)
+// {
+//     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+// }
 
 void setup() {
   // Set Arduino board pin 13 to output
@@ -15,6 +25,16 @@ void setup() {
   
   MIDI.setHandleNoteOn(handleNoteOn); 
   MIDI.setHandleNoteOff(handleNoteOff);
+  
+  Serial.begin(9600);
+  
+  Serial.print("Value start at: ");
+  Serial.println(ramps[0].value());
+
+  Serial.print("Strating interpolation");
+  
+  // start interpolation (value to go to, duration)
+  ramps[0].go(0, 0);                
 }
 
 // Main loop
@@ -24,6 +44,7 @@ void loop() {
 
 void handleNoteOn(byte channel, byte pitch, byte velocity) { 
   digitalWrite(LED,HIGH);  //Turn LED on
+  ramps[0].go(velocity, 1000);
 }
 
 // * A NOTE ON message with Velocity = 0 will be treated as a NOTE OFF message *
